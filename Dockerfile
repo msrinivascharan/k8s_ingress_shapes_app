b922152e9c132d1d2ba045e581aaec1bc47fa8ac
Srinivas charan mamidi <msrinivascharan@gmail.com>
2020-04-29 19:56:14 +0530

    ingress resource added for shapes app

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3
 ADD helloworld.py /
 RUN pip install flask
 RUN pip install flask_restful
-EXPOSE 3337
+EXPOSE 3334
 CMD [ "python", "./helloworld.py"]
--- helloworld.py
+++ helloworld.py
@@ -6,9 +6,9 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        #return 'Hexagone-a plane figure with six straight sides and angles.'
-        return 'This is invalid request handled by default backend, check your request path and try again'
+        return 'Square-a plane figure with four equal straight sides and four right angles.'
+        #return 'This is invalid request handled by default backend, check your request path and try again'
 api.add_resource(Greeting, '/') # Route_1
 
 if __name__ == '__main__':
-    app.run('0.0.0.0','3337')
+    app.run('0.0.0.0','3334')
